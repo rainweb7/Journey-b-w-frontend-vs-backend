afb561ebf01fe88b6f8bc84e084bdfe42b2a9a2c
rainweb7 <spacemusic208@gmail.com>
2025-12-23 21:15:36 +0530

    Add of jobs.html file

--- app.js
+++ app.js
@@ -1,75 +1,38 @@
-//Login
-function login(){
-    let email=document.getElementById("email").value;
-    let pass=document.getElementById("password").value;
-    let error=document.getElementById("error");
+console.log("JS loaded");
 
-    if(email  ==="" || pass ===""){
-        error.innerText="All fields required";
-    }else{
-        window.location.href="jobs.html";
-    }
-}
+document.addEventListener("DOMContentLoaded", function () {
 
-//Job data
-const jobs=[
+  console.log("DOM fully loaded");
+
+  const jobList = document.getElementById("job-list");
+  console.log("jobList:", jobList);
+
+  const jobs = [
     {
-        id: 1,
-        title: "Frontend Developer",
-        company: "Tech Solutions",
-        location: "Chennai",
-        desc: "HTML, CSS, JavaScript, React",
+      id: 1,
+      title: "Frontend Developer",
+      company: "Zoho",
+      location: "Chennai"
     },
     {
-        id: 2,
-        title: "backend Developer",
-        company: "Infosys",
-        location: "Bangalore",
-        desc: "Java, SQL, DSA",
+      id: 2,
+      title: "Software Engineer",
+      company: "Infosys",
+      location: "Bangalore"
     }
-];
+  ];
 
-//Job List
-if(document.getElementById("job-list")){
-    const list=document.getElementById("job-list");
+  jobs.forEach(job => {
+    const div = document.createElement("div");
+    div.className = "job";
 
-    jobs.forEach(job => {
-        const div = document.createElement("div");
-        div.className="job-card";
-        div.innerHTML=`
-        <h3>${job.title}</h3>
-        <p>${job.company} - ${job.location}</p>
-        <button onclick="viewJob(${job.id})">View</button>
-        `;
-        list.appendChild(div);
-    });
-}
-
-function viewJob(id){
-    window.location.href=`job.html?id=${id}`;
-}
-
-//Job Details
-if(document.getElementById("job-details")){
-    const params=new URLSearchParams(window.location,search);
-    const id=params.get("id");
-
-    const job=jobs.find(j=>j.id==id);
-    document.getElementById("job-details").innerHTML=`
-      <h2>${job.title}</h2>
-      <p>${job.company}</p>
-      <p>${job.desc}</p>
-      <button onclick="applyJob()">Apply</button>
+    div.innerHTML = `
+      <h3>${job.title}</h3>
+      <p>${job.company} - ${job.location}</p>
+      <button>View</button>
     `;
-}
 
-function applyJob(){
-    window.location.href="apply.html";
-}
+    jobList.appendChild(div);
+  });
 
-//Apply
-function apply(){
-    document.getElementById("success").innerText=
-    "Application submitted successfully!";
-}
-
+});
