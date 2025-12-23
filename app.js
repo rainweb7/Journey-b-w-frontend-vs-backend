866018a8d25b6e019c4eb2c86050a56c2edfc3cc
rainweb7 <spacemusic208@gmail.com>
2025-12-23 20:13:55 +0530

    Add of js file

--- app.js
+++ app.js
@@ -1,1 +1,75 @@
+//Login
+function login(){
+    let email=document.getElementById("email").value;
+    let password=document.getElementById("password").value;
+    let error=document.getElementById("error");
+
+    if(email==="" || pass ===""){
+        error.innerText="All fields required";
+    }else{
+        window.location.href="jobs.html";
+    }
+}
+
+//Job data
+const jobs=[
+    {
+        id: 1,
+        title: "Frontend Developer",
+        company: "Tech Solutions",
+        location: "Chennai",
+        desc: "HTML, CSS, JavaScript, React",
+    },
+    {
+        id: 2,
+        title: "backend Developer",
+        company: "Infosys",
+        location: "Bangalore",
+        desc: "Java, SQL, DSA",
+    }
+];
+
+//Job List
+if(document.getElementById("job-list")){
+    const list=document.getElementById("job-list");
+
+    jobs.forEach(job => {
+        const div = document.createElement("div");
+        div.className="job-card";
+        div.innerHTML=`
+        <h3>${job.title}</h3>
+        <p>${job.company} - ${job.location}</p>
+        <button onclick="viewJob(${job.id})">View</button>
+        `;
+        list.appendChild(div);
+    });
+}
+
+function viewJob(id){
+    window.location.href=`job.html?id=${id}`;
+}
+
+//Job Details
+if(document.getElementById("job-details")){
+    const params=new URLSearchParams(window.location,search);
+    const id=params.get("id");
+
+    const job=jobs.find(j=>j.id==id);
+    document.getElementById("job-details").innerHTML=`
+      <h2>${job.title}</h2>
+      <p>${job.company}</p>
+      <p>${job.desc}</p>
+      <button onclick="applyJob()">Apply</button>
+    `;
+}
+
+function applyJob(){
+    window.location.href="apply.html";
+}
+
+//Apply
+function apply(){
+    document.getElementById("success").innerText=
+    "Application submitted successfully!";
+}
 
